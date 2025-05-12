5a8808723d6068a7722400f636d0bb5fc041dde4
homemix <kenmutati@gmail.com>
2025-05-12 08:17:36 +0300

    ADD: Apply Aggregations and Window Functions.

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,8 @@
+SELECT count(bookings.user_id) FROM bookings
+GROUP BY bookings.user_id;
+
+SELECT properties.* ,
+       RANK() over (ORDER BY  COUNT(bookings.booking_id) DESC ) AS bookings_total
+FROM properties
+JOIN bookings ON properties.property_id = bookings.property_id
+GROUP BY  properties.property_id;
