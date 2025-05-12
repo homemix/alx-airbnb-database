3309c903e39929fd7928ef0db70068b94d44450f
homemix <kenmutati@gmail.com>
2025-05-12 07:57:56 +0300

    ADD: Practice Subqueries

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,12 +1,14 @@
 SELECT properties.*
-from properties
-         JOIN reviews
-              ON properties.property_id = reviews.property_id
-where reviews.rating > 4.0;
+FROM properties
+WHERE properties.property_id IN (SELECT property_id
+                                 FROM reviews
+                                 GROUP BY property_id
+                                 HAVING AVG(rating) > 4.0);
+
 
 
 SELECT users.*
 FROM users
-WHERE (SELECT count(*)
+WHERE (SELECT COUNT(*)
        FROM bookings
        WHERE bookings.booking_id = users.user_id) > 3;
