b0a51d5108a5691e71264dacdd0ef0920c6bfd00
homemix <kenmutati@gmail.com>
2025-05-12 08:21:44 +0300

    ADD: Apply Aggregations and Window Functions - row number.

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -2,7 +2,8 @@ SELECT count(bookings.user_id) FROM bookings
 GROUP BY bookings.user_id;
 
 SELECT properties.* ,
-       RANK() over (ORDER BY  COUNT(bookings.booking_id) DESC ) AS bookings_total
+       RANK() over (ORDER BY  COUNT(bookings.booking_id) DESC ) AS bookings_rank,
+       ROW_NUMBER() over (ORDER BY COUNT(bookings.booking_id) DESC ) AS boookings_number
 FROM properties
 JOIN bookings ON properties.property_id = bookings.property_id
 GROUP BY  properties.property_id;
