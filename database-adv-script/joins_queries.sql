ac75a57d8d5fc8d7eaa5e2be6c715f8812f6feed
homemix <kenmutati@gmail.com>
2025-05-12 07:35:41 +0300

    EDIT: SQL dialect to use capital letters.

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -3,8 +3,9 @@ INNER JOIN users   ON bookings.user_id = users.user_id;
 
 SELECT  properties.*,reviews.* FROM properties
 LEFT JOIN  reviews
-ON properties.property_id = reviews.property_id;
+ON properties.property_id = reviews.property_id
+ORDER BY properties.created_at;
 
 SELECT users.*,bookings.* FROM users
-LEFT OUTER JOIN  bookings
+FULL OUTER JOIN  bookings
 ON users.user_id = bookings.user_id;
