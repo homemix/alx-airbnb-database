d7e68cbd6eea8ab0e3a2c003b9ec6fd3a7e7e0b8
homemix <kenmutati@gmail.com>
2025-05-12 09:44:48 +0300

    ADD: Optimize Complex Queries

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -13,4 +13,5 @@ LEFT JOIN payments ON bookings.booking_id = payments.booking_id;
 SELECT bookings.*,users.*,properties.*,payments.* FROM bookings
  JOIN users ON bookings.user_id = users.user_id
  JOIN properties ON bookings.property_id = properties.property_id
-LEFT JOIN payments ON bookings.booking_id = payments.booking_id;
+LEFT JOIN payments ON bookings.booking_id = payments.booking_id
+WHERE bookings.status = 'confirmed' AND payments.payment_method = 'paypal' ;
