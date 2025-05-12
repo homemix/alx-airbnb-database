52ffd943cd4845349d255acae750c18240918f72
homemix <kenmutati@gmail.com>
2025-05-12 07:27:20 +0300

    EDIT: Write Complex Queries with Joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,10 +1,10 @@
 select bookings.*,users.* from bookings
 inner join users   on bookings.user_id = users.user_id;
 
-select  properties.* from properties
+select  properties.*,reviews.* from properties
 left join reviews
 on properties.property_id = reviews.property_id;
 
-select users.* from users
+select users.*,bookings.* from users
 left outer join bookings
 on users.user_id = bookings.user_id;
